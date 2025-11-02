20494874255ca69fa2ad01a1bd33a9c0dc56ee2b
GunturPrastyo <prastyoguntur982@gmail.com>
2025-11-02 18:41:01 +0700

    layout1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "./providers";
+import { ThemeProvider } from "@/app/providers";
 
 // Registrasi font dari Google
 const poppins = Poppins({
