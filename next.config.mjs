ade00b32b8426e633542129cd11436388183cc69
prastyoguntur982-hub <prastyoguntur982@students.unnes.ac.id>
2025-11-02 11:09:23 +0700

    Skip TS3

--- next.config.mjs
+++ next.config.mjs
@@ -28,14 +28,6 @@ const nextConfig = {
       },
     ],
   },
-
-  // ✅ Ini harus sejajar, bukan di dalam images
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
